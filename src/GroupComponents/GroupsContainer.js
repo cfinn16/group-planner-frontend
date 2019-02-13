ae3b432d7f580fa8b513878e3c2c88d353faf2a2
jmartinez729 <jmartinez729@gmail.com>
2019-02-13 11:17:34 -0500

    added some styling

--- src/GroupComponents/GroupsContainer.js
+++ src/GroupComponents/GroupsContainer.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import Group from './Group.js'
-
+import {SideNavItem} from 'react-materialize'
 
 
 // class App extends Component {
 class GroupsContainer extends React.Component {
 
-  // state = {
-  //   isClicked: false
-  // }
 
   renderGroups = (arr, fn) => {
-    console.log("in GroupsContainer renderGroups: ", arr)
+    // console.log("in GroupsContainer renderGroups: ", arr)
+
     return arr.map( g => {
       return (
         <Group
@@ -20,13 +18,21 @@ class GroupsContainer extends React.Component {
         handleOnClickGroups={fn}
         selectedGroupId={this.props.selectedGroupId}
         />
-      )
-    })
+    ) // end of return
+  }) // end of return arr.map
   } // end of renderGroups
 
   render() {
     return (
-      this.props.data ? this.renderGroups(this.props.data.groups, this.props.handleOnClickGroups) : <h1>No data</h1>
+
+      <div>
+        <h3>My Groups</h3>
+          <button onClick={this.props.handleAddGroup}>+</button>
+          <SideNavItem divider />
+
+      {this.props.data ? this.renderGroups(this.props.data.groups, this.props.handleOnClickGroups) : <h1>No data</h1>}
+      </div>
+
       // props.data ? <h1>{console.log("InGroupsContainer: ", props.data.groups)}</h1> : <h1>No data</h1>
     ) // end of return
   } // end of render()
--- src/GroupComponents/NewGroup.js
+++ src/GroupComponents/NewGroup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Input, Row} from 'react-materialize'
 
 // const showAllUsers = (input) => {
 //   return input.allUsers.map(user => {
@@ -10,15 +11,18 @@ const NewGroup = (props) => {
 
   return (
     <div>
-      <form onSubmit={(event) => props.handleNewGroupSubmit(event)} id="newGroup">
-        <input onChange={(event) => props.handleNewGroupNameChange(event)} type="text" name="groupName" placeholder="Group Name" value={props.newGroupName} /><br />
-        <select multiple onChange={(event => props.handleUserSelect(event))}>
-          {props.allUsers.map(user => {
-            return <option value={user.id}>{user.name}</option>
-          })}
-        </select><br />
 
-        <input type="submit" value="Create Group" />
+      <form onSubmit={(event) => props.handleNewGroupSubmit(event)} id="newGroup">
+        <Row>
+          <Input onChange={(event) => props.handleNewGroupNameChange(event)} type="text" name="groupName" placeholder="Group Name" value={props.newGroupName} /><br />
+          <Input type="select" label="Materialize Select" multiple onChange={(event => props.handleUserSelect(event))}>
+            {props.allUsers.map(user => {
+              return <option value={user.id}>{user.name}</option>
+            })}
+          </Input><br />
+
+          <Input type="submit" value="Create Group" />
+        </Row>
       </form>
 
 
